c4bfdca8a6143692dd517e2d35424b52ba3a2508
Vaibhav Patel <vp932712@gmail.com>
2017-12-19 21:31:28 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,7 +2,7 @@ def dictionary(word)
 
   sub = {"hello" =>'hi',
 "to" => "2", "two"=>"2", "too"=>'2',
-"for"=>"4","four"=>'4',
+"for"=>"4","four"=>'4', "For" => "4",
 'be' => 'b',
 'you' => 'u',
 "at" =>"@","and" => "&"}
