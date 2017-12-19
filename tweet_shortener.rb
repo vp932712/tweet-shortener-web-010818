d27fd12c25c8982aeae1d7a169023bae3532e1a1
Vaibhav Patel <vp932712@gmail.com>
2017-12-19 21:25:00 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def dictionary(word)
+  
+  sub = {"hello" =>'hi',
+"to" => "2", "two"=>"2", "too"=>'2', 
+"for"=>"4","four"=>'4',
+'be' => 'b',
+'you' => 'u',
+"at" =>"@","and" => "&"}
+
+if sub[word] == nil 
+  word
+else
+  sub[word]
+end
+
+end
+
+
+
+
+
+
+def word_substituter(str)
+  empty = []
+  arr = str.split(" ")
+  arr.each do |word|
+    empty.push(dictionary(word))
+  end
+  empty.join(" ")
+end
